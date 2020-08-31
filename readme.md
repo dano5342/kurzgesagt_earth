21d22762ad78bdc53de489435052506c56e9dccf
dano5342 <ellis.daniel0@gmail.com>
2020-08-31 16:40:02 +0200

    fixed clouds not going above earth lines, js added to append stars.

--- readme.md
+++ readme.md
@@ -1,4 +1,6 @@
 # Kurzgesagt Earth
+
+
+A repository for me to have a go at recreating the amazing animated earth found on Kurzgesagt's youtube channel. Whilst also following a small codepen instructional [here](https://dev.to/moniet/kurzgesagt-earth-a-css-recreation-23b3)
 ---
-A repository for me to have a go at recreating the amazing animated earth found on Kurzgesagt's youtube channel.
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,29 @@
-// Empty for now as this will be used when the illustration is complete for animation
+let smallStars = 25;
+let largeStars = 10;
+let container = document.querySelector('.bg');
+
+function randomVal(max, min) {
+    return Math.floor((Math.random() * (max - min)) + min);
+}
+
+for (let i = 0; i < smallStars; i++) {
+    let star = document.createElement('div');
+
+    star.className = 'stars';
+    star.style.height = randomVal(2, 1) + 'px';
+    star.style.width = randomVal(2, 1) + 'px';
+    star.style.top = randomVal(100, 5) + 'vh';
+    star.style.left = randomVal(100, 5) + 'vw';
+    container.appendChild(star);
+}
+
+for (let i = 0; i < largeStars; i++) {
+    let star = document.createElement('div');
+
+    star.className = 'large-stars';
+    star.style.height = randomVal(5, 4) + 'px';
+    star.style.width = randomVal(5, 4) + 'px';
+    star.style.top = randomVal(80, 5) + 'vh';
+    star.style.left = randomVal(80, 5) + 'vw';
+    container.appendChild(star);
+}
